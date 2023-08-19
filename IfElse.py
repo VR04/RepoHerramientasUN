96c3fe64a2dc65c1291b91eb042452f3b8912f65
SergioVR <sergiovargas042001@gmail.com>
2023-08-19 10:52:01 -0500

    ParabolicPath

---
+++ IfElse.py
@@ -0,0 +1,27 @@
+'''Age = 12
+
+message= "Elegible" if Age>=18 else "Not Elegible"
+
+print(message)
+
+
+'''
+
+'''if 10 == "10":
+    print("a")
+elif "bag">"apple" and "bag" > "cat":
+    print("b")
+else: 
+    print("c")'''
+
+'''values=["a", "b","c"]
+
+for count in range(len(values)):
+     value = values[count]
+     print(count, value)
+for count, value in enumerate(values):
+    print(count, value)
+'''
+
+for ii in range(3,0,-1):
+    print(2*ii+1)  
